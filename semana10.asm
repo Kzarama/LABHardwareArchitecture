ae5d849a9e02b7a773732019c5d83d5388a58b01
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-03-26 13:33:48 -0500

    lab finished

--- semana10.asm
+++ semana10.asm
@@ -2,29 +2,24 @@
 .model flat, stdcall
 option casemap : none
 .data
-IPHeader byte 45h, 00h,00h, 29h, 36h, 27h, 40h, 00h, 80h, 06h, 0d9h, 70h, 0c0h, 0a8h, 01h, 02h, 0dh, 58h, 1ch, 35h
+IPHeader byte 4fh, 00h, 00h, 29h, 36h, 27h, 40h, 00h, 80h, 06h, 0d9h, 70h, 0c0h, 0a8h, 01h, 02h, 0dh, 58h, 1ch, 35h
 
 .code
 start:
     mov esi,0
     xor bx,bx
-L11:mov ax,word ptr[IPHeader+esi]; AX : AH AL
+L1:
+    mov ax,word ptr[IPHeader+esi]; AX : AH AL
     xchg al,ah 
     add esi,2
+    not ax
     add bx,ax
+    jnc L2
+    add bx, 1
+L2:
     cmp esi,20
-    jne L11
+    jne L1
     not bx
-    
     xor eax, eax
     ret
     end start
-    
-%include "io.inc"
-
-section .text
-global CMAIN
-CMAIN:
-    ;write your code here
-    xor eax, eax
-    ret
--- suma_biginteger.asm
+++ suma_biginteger.asm
@@ -1,22 +1,45 @@
 %include "io.inc"
 
-section.data
-BD_A db "1567.34"
-BD_B db "6232.53"
-BD_C db "0000.00"
+section .data
+BD_A db "186976.35"
+BD_B db "425731.74"
+BD_C db "000000.00"
 
 section .text
 global CMAIN
 CMAIN:
     mov ebp, esp; for correct debugging
-    ;write your code here
+    
+    ;colocar las pocisiones en 0
     xor eax, eax
     xor ebx, ebx
     xor ecx, ecx
+    xor edx, edx
     
-    mov al, [BD_A+6]
-    mov bl, [BD_B+6]
-    add al. bl
-    mov [BD_C+6], al
-    xor eax, eax
+    ;tamaño de los numeros
+    mov ecx, 8
+    
+    ;suma
+L18:mov al, [BD_A + ecx];tomar numero del registro
+    mov bl, [BD_B + ecx];tomar numero del registro
+    cmp al, '.';compara si es punto
+    je L25;salta si es punto
+    add al, bl;suma numeros
+    AAA;arregla suma
+    add al, dl;sumar carry
+    AAA;arregla suma 
+    
+    mov dl, 0;borrar carry
+    add al, 30h;sumar 30 para que se arregle el numero con cod ascii
+    
+    cmp ah, 0;compara si tiene carry
+    je L25;salta si no tiene carry
+    add dl, 1h;añade el carry a otra posicion
+    mov ah, 0;borra el carry
+L25:mov [BD_C + ecx], al;coloca la suma en la posicion del registro que debe ir
+    dec ecx;decrementa contador
+    cmp ecx, -1;compara si hay numeros en el arreglo
+    jne L18;salta si hay numeros
+    
+    xor eax,eax
     ret
