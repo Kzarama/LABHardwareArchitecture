ffecf2e9fb7295c8d687ec34628865cac6296730
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-04-22 17:18:45 -0500

    work in house finished week 13

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"C:\Users\kz\Downloads\TimeMultInt1000.txt");
+                int n = 1000;
+
+                int[,] matriz = new int[n, n];
+
+                for (int i = 0; i < 100; i++)
+                {
+
+                    sw.WriteLine(MultiplicacionInt(matriz, n));
+                    //sw.WriteLine(sumaIntLocal());
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+
+        }
+
+        public static double MultiplicacionInt(int[,] matriz, int n)
+        {
+
+            int Mul = 0;
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Restart();
+            sw.Start();
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+
+                    Mul *= matriz[i, j];
+
+                }
+
+            }
+
+            sw.Stop();
+
+            long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+
+            return tiempo;
+        }
+
+    }
+}
+
