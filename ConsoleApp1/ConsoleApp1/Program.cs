8e26a37dcd683cf37f93bc6b0f2f5bbd4b8a3700
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-05-11 16:09:15 -0500

    lab week 13 uploaded

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,26 +5,23 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ConsoleApp4
+namespace ConsoleAppWeek13
 {
-    class Program
+    class ProgramLabSem13_gr103
     {
         public static void Main(string[] args)
         {
-
             try
             {
-                StreamWriter sw = new StreamWriter(@"C:\Users\kz\Downloads\TimeMultInt1000.txt");
-                int n = 1000;
+                StreamWriter sw = new
+                StreamWriter(@"C:\Users\kz\Documents\git\LABHardwareArchitecture\ConsoleApp1\TimeSem13_1000local.txt");
 
-                int[,] matriz = new int[n, n];
-
+                int n = 3000;
+                //int[,] matriz = new int[n, n]; //para MultIntvarGlobal()
                 for (int i = 0; i < 100; i++)
                 {
-
-                    sw.WriteLine(MultiplicacionInt(matriz, n));
-                    //sw.WriteLine(sumaIntLocal());
+                    //sw.WriteLine(MultIntvarGlobal(matriz,n)); //variable global
+                    sw.WriteLine(MultIntvarLocal()); //variable local
                 }
                 sw.Close();
             }
@@ -32,39 +29,42 @@ namespace ConsoleApp4
             {
                 Console.WriteLine("Exception: " + e.Message);
             }
-
-
         }
-
-        public static double MultiplicacionInt(int[,] matriz, int n)
+        public static double MultIntvarGlobal(int[,] matriz, int n)
         {
-
-            int Mul = 0;
-
+            int mul = 1;
             Stopwatch sw = new Stopwatch();
-
             sw.Restart();
             sw.Start();
-
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
-
                 for (int j = 0; j < matriz.GetLength(0); j++)
                 {
-
-                    Mul *= matriz[i, j];
-
+                    mul *= matriz[j, i];
                 }
-
             }
-
             sw.Stop();
-
             long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
-
             return tiempo;
         }
-
+        public static double MultIntvarLocal()
+        {
+            int n = 1000; //<--defina aquí el tamaño de la matriz
+            int[,] matriz = new int[n, n];
+            int mul = 1;
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            sw.Start();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    mul *= matriz[j,i];
+                }
+            }
+            sw.Stop();
+            long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+            return tiempo; // Math.Pow(n, 2);
+        }
     }
 }
-
